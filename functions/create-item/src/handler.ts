533a84224413cb6494ad06c36a2a4d91ee09d621
AustinCabs <austinbrien.cabanada@gmail.com>
2026-02-24 23:28:14 +0800

    test event log, remove spread

--- functions/create-item/src/handler.ts
+++ functions/create-item/src/handler.ts
@@ -7,7 +7,7 @@ import { createItem } from "common/services/dynamodb";
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   const requestId = event.requestContext?.requestId;
   // info("Event", { event })
-  info("Create item requested", { requestId, ...event });
+  info("Create item requested", { requestId, event });
 
   const body = parseJson<Record<string, unknown>>(event.body);
   const validation = validateCreateItemBody(body);
